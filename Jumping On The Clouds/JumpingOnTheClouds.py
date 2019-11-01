1985431abbf54cdef66d9aa3b031e990ac7df753
damarhardy <damarhardy@gmail.com>
2019-11-01 15:28:45 +0700

    Jumping On The Cloud Solution Delivered

--- Jumping On The Clouds/JumpingOnTheClouds.py
+++ Jumping On The Clouds/JumpingOnTheClouds.py
@@ -8,6 +8,24 @@ import sys
 
 # Complete the jumpingOnClouds function below.
 def jumpingOnClouds(c):
+    n = len(c)
+    i = 0
+    count=0
+    while i < n+1:
+        if i+2 < n and c[i+2]!=1:
+            # print ("2 Langkah")
+            i=i+2
+            count = count +1
+        elif i+1<n and c[i+1]!=1:
+            # print ("1 Langkah")
+            i=i+1
+            count = count+1
+        else:
+            break
+
+        # print count
+
+    return count
 
 if __name__ == '__main__':
     os.environ['OUTPUT_PATH'] = 'OUTPUT.txt'
@@ -18,6 +36,7 @@ if __name__ == '__main__':
     c = map(int, raw_input().rstrip().split())
 
     result = jumpingOnClouds(c)
+    print(result)
 
     fptr.write(str(result) + '\n')
 
